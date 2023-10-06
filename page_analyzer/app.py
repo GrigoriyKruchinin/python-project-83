fdb70061c5f61df9efaabd84ebcd0fe7d72a71ec
GrigoriyKruchinin <GrigoriyKruchinin>
2023-10-06 12:50:08 +0200

    add index page

---
+++ page_analyzer/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['DATABASE_URL'] = os.getenv('DATABASE_URL')
+app.config['DEBUG'] = os.getenv('DEBUG')
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
