317145205050aa6aeb0688d930cfe74a339845e8
GrigoriyKruchinin <GrigoriyKruchinin>
2023-10-12 14:20:22 +0200

    create class DatabaseConnection for db

--- page_analyzer/db.py
+++ page_analyzer/db.py
@@ -1,8 +1,8 @@
 from psycopg2 import connect
 from psycopg2.extras import NamedTupleCursor
-from datetime import datetime
 import os
 from dotenv import load_dotenv
+from datetime import datetime
 
 
 load_dotenv()
@@ -11,79 +11,82 @@ load_dotenv()
 DATABASE_URL = os.getenv('DATABASE_URL')
 
 
+class DatabaseConnection:
+    def __enter__(self):
+        self.connection = connect(DATABASE_URL)
+        self.cursor = self.connection.cursor(cursor_factory=NamedTupleCursor)
+        return self.cursor
+
+    def __exit__(self):
+        self.cursor.close()
+        self.connection.commit()
+        self.connection.close()
+
+
 def add_url_into_db(url):
-    query = 'INSERT INTO urls (name, created_at) VALUES (%s, %s)'
-    values = (url, datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
-    with connect(DATABASE_URL) as db:
-        with db.cursor(cursor_factory=NamedTupleCursor) as cursor:
-            cursor.execute(query, values)
+    with DatabaseConnection() as cursor:
+        query = 'INSERT INTO urls (name, created_at) VALUES (%s, %s)'
+        values = (url, datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+        cursor.execute(query, values)
 
 
 def get_url_by_name(url):
-    query = 'SELECT * FROM urls WHERE name = (%s)'
-    with connect(DATABASE_URL) as db:
-        with db.cursor(cursor_factory=NamedTupleCursor) as cursor:
-            cursor.execute(query, (url,))
-            data = cursor.fetchone()
-
-    return data
+    with DatabaseConnection() as cursor:
+        query = 'SELECT * FROM urls WHERE name = (%s)'
+        cursor.execute(query, (url,))
+        data = cursor.fetchone()
+        return data
 
 
 def get_url_by_id(id):
-    query = 'SELECT * FROM urls WHERE id = (%s)'
-    with connect(DATABASE_URL) as db:
-        with db.cursor(cursor_factory=NamedTupleCursor) as cursor:
-            cursor.execute(query, (id,))
-            data = cursor.fetchone()
-
-    return data
+    with DatabaseConnection() as cursor:
+        query = 'SELECT * FROM urls WHERE id = (%s)'
+        cursor.execute(query, (id,))
+        data = cursor.fetchone()
+        return data
 
 
 def add_url_checks(checks):
-    query = (
-        'INSERT INTO url_checks '
-        '(url_id, status_code, h1, title, description, created_at) '
-        'VALUES (%s, %s, %s, %s, %s, %s)'
-    )
-    values = (
-        checks.get('url_id'),
-        checks.get('status_code'),
-        checks.get('h1', ''),
-        checks.get('title', ''),
-        checks.get('description', ''),
-        datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    )
-
-    with connect(DATABASE_URL) as db:
-        with db.cursor(cursor_factory=NamedTupleCursor) as cursor:
-            cursor.execute(query, values)
+    with DatabaseConnection() as cursor:
+        query = (
+            'INSERT INTO url_checks '
+            '(url_id, status_code, h1, title, description, created_at) '
+            'VALUES (%s, %s, %s, %s, %s, %s)'
+        )
+        values = (
+            checks.get('url_id'),
+            checks.get('status_code'),
+            checks.get('h1', ''),
+            checks.get('title', ''),
+            checks.get('description', ''),
+            datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        )
+        cursor.execute(query, values)
 
 
 def get_checks_by_url_id(id):
-    query = 'SELECT * FROM url_checks WHERE url_id=(%s) ORDER BY id DESC'
-    with connect(DATABASE_URL) as db:
-        with db.cursor(cursor_factory=NamedTupleCursor) as cursor:
-            cursor.execute(query, (id,))
-            checks = cursor.fetchall()
-    return checks
+    with DatabaseConnection() as cursor:
+        query = 'SELECT * FROM url_checks WHERE url_id=(%s) ORDER BY id DESC'
+        cursor.execute(query, (id,))
+        checks = cursor.fetchall()
+        return checks
 
 
 def get_all_urls():
-    query = (
-        'SELECT '
-        'urls.id AS id, '
-        'urls.name AS name, '
-        'url_checks.created_at AS last_check, '
-        'status_code '
-        'FROM urls '
-        'LEFT JOIN url_checks '
-        'ON urls.id = url_checks.url_id '
-        'AND url_checks.id = ('
-        'SELECT max(id) FROM url_checks WHERE urls.id = url_checks.url_id) '
-        'ORDER BY urls.id DESC;'
-    )
-    with connect(DATABASE_URL) as db:
-        with db.cursor(cursor_factory=NamedTupleCursor) as cursor:
-            cursor.execute(query)
-            urls = cursor.fetchall()
-    return urls
+    with DatabaseConnection() as cursor:
+        query = (
+            'SELECT '
+            'urls.id AS id, '
+            'urls.name AS name, '
+            'url_checks.created_at AS last_check, '
+            'status_code '
+            'FROM urls '
+            'LEFT JOIN url_checks '
+            'ON urls.id = url_checks.url_id '
+            'AND url_checks.id = ('
+            'SELECT max(id) FROM url_checks WHERE urls.id = url_checks.url_id) '
+            'ORDER BY urls.id DESC;'
+        )
+        cursor.execute(query)
+        urls = cursor.fetchall()
+        return urls
