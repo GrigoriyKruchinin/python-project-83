3108244d1c72aa903a5eb0a4a3f394963a816d38
GrigoriyKruchinin <GrigoriyKruchinin>
2023-10-13 11:31:10 +0200

    update html_parser

--- page_analyzer/utilities/html_parser.py
+++ page_analyzer/utilities/html_parser.py
@@ -1,9 +1,9 @@
 from bs4 import BeautifulSoup
 
 
-def parse_page(page_text, max_length=255):
+def parse_page(content, max_length=255):
     checks = {}
-    soup = BeautifulSoup(page_text, 'html.parser')
+    soup = BeautifulSoup(content, 'html.parser')
 
     h1_tag = soup.find('h1')
     checks['h1'] = h1_tag.get_text().strip() if h1_tag else ''
@@ -13,9 +13,9 @@ def parse_page(page_text, max_length=255):
     checks['title'] = title_tag.string.strip() if title_tag else ''
     checks['title'] = checks['title'][:max_length]
 
-    all_metas = soup.find_all('meta', attrs={'name': 'description'})
-    if all_metas:
-        checks['description'] = all_metas[0].get('content', '').strip()
+    description_tag = soup.find('meta', attrs={'name': 'description'})
+    if description_tag:
+        checks['description'] = description_tag.get('content', '').strip()
         checks['description'] = checks['description'][:max_length]
     else:
         checks['description'] = ''
