17be1f22b71bc84d69502a14be880f00de59015f
GrigoriyKruchinin <GrigoriyKruchinin>
2023-10-19 12:20:08 +0200

    add constant MAX_URL_LENGHT to url.py

--- page_analyzer/url.py
+++ page_analyzer/url.py
@@ -2,12 +2,15 @@ from urllib.parse import urlparse
 import validators
 
 
+MAX_URL_LENGHT = 255
+
+
 def validator(url):
     error = None
     if not url:
         error = 'URL обязателен'
-    elif len(url) > 255:
-        error = 'URL превышает 255 символов'
+    elif len(url) > MAX_URL_LENGHT:
+        error = f'URL превышает {MAX_URL_LENGHT} символов'
     elif not validators.url(url):
         error = 'Некорректный URL'
     return error
